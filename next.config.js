8e2df783cfe66b7ae2a41e3ce8fb73e68a081b05
zhenzhifeng <1048157455@qq.com>
2024-01-02 14:43:36 +0800

    ignoreDuringBuilds

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     appDir: true,
   },
   images: {
     domains: [
-      'res.cloudinary.com', 
+      'res.cloudinary.com',
       'avatars.githubusercontent.com',
       'lh3.googleusercontent.com'
     ]
