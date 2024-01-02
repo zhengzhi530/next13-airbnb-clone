f3ecda99d7a2e3a3c62d8b419e9df1fe51bb980a
zhenzhifeng <1048157455@qq.com>
2024-01-02 11:28:49 +0800

    React fragment

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -25,20 +25,22 @@ const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">Op
 
 const Map: React.FC<MapProps> = ({ center }) => {
   return (
-    <MapContainer
-      center={center as L.LatLngExpression || [51, -0.09]}
-      zoom={center ? 4 : 2}
-      scrollWheelZoom={false}
-      className="h-[35vh] rounded-lg"
-    >
-      <TileLayer
-        url={url}
-        attribution={attribution}
-      />
-      {center && (
-        <Marker position={center as L.LatLngExpression} />
-      )}
-    </MapContainer>
+    <>
+      <MapContainer
+        center={center as L.LatLngExpression || [51, -0.09]}
+        zoom={center ? 4 : 2}
+        scrollWheelZoom={false}
+        className="h-[35vh] rounded-lg"
+      >
+        <TileLayer
+          url={url}
+          attribution={attribution}
+        />
+        {center && (
+          <Marker position={center as L.LatLngExpression} />
+        )}
+      </MapContainer>
+    </>
   )
 }
 
